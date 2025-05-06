ee5914de849b24d5c3945aa3c8548a869bdf233e
diala2 <dialafayoumi@gmail.com>
2025-05-06 15:10:48 +0300

    Initial Commit

---
+++ zign/zignApp.swift
@@ -0,0 +1,17 @@
+//
+//  zignApp.swift
+//  zign
+//
+//  Created by Diala Abdulnasser Fayoumi on 08/11/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct zignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
